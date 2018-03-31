f757999d891c00218869945b24160c54e4feaee3
Tito Titov <tima_t@Titos-MacBook-Pro.local>
2018-03-31 11:51:49 +0300

    navigation signin

---
+++ LatensPay/navigation/RootNavigation.js
@@ -0,0 +1,43 @@
+import { StackNavigator, TabNavigator } from "react-navigation";
+import React, {Component} from 'react';
+
+import SignUp from "../screens/SignUp";
+import SignIn from "../screens/SignIn";
+import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+
+export const SignedOut = StackNavigator({
+  SignUp: {
+    screen: SignUp,
+    navigationOptions: {
+      title: "Sign Up"
+    }
+  },
+  SignIn: {
+    screen: SignIn,
+    navigationOptions: {
+      title: "Sign In"
+    }
+  }
+});
+
+export const SignedIn = TabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name="home" size={30} color={tintColor} />
+      )
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: "Profile",
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name="user" size={30} color={tintColor} />
+      )
+    }
+  }
+});
