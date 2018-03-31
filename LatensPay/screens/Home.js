c722a6b270b4ac5bb618bc4dc08c9064672d6ea3
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-03-31 12:26:35 +0300

    yarn.lock updated and home screen code formated

--- LatensPay/screens/Home.js
+++ LatensPay/screens/Home.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+
 export default ({ navigation }) => (
     <View style={{ paddingVertical: 20 }}>
         <Text> Home </Text>
     </View>
-  );
+);
