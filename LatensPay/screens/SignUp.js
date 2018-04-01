d20e064043a3f9a5f838257d83a342a973ee2b65
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-04-01 12:27:34 +0300

    Fixing the image warning and design updated as well in profile

--- LatensPay/screens/SignUp.js
+++ LatensPay/screens/SignUp.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import { Container, Label, Form, Item, Content, Button, Input, Text } from 'native-base';
-import { postCall } from '../services/Http';
+import React, {Component} from 'react';
+import {Button, Container, Content, Form, Input, Item, Text} from 'native-base';
+import {postCall} from '../services/Http';
 
 export default class SignUp extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            password:'',
+            password: '',
             email: ''
         }
     }
 
-    handleUsername(username){
+    handleUsername(username) {
         this.setState({username});
     }
 
-    handlePassword(password){
+    handlePassword(password) {
         this.setState({password});
     }
 
-    handleEmail(email){
+    handleEmail(email) {
         this.setState({email});
     }
 
@@ -29,25 +29,34 @@ export default class SignUp extends Component {
                 <Content>
                     <Form>
                         <Item>
-                            <Input onChangeText = {this.handleUsername.bind(this)} placeholder="Username" />
+                            <Input onChangeText={this.handleUsername.bind(this)} placeholder="Username"/>
                         </Item>
                         <Item>
-                            <Input secureTextEntry={true} onChangeText = {this.handlePassword.bind(this)}  placeholder="Password" />
+                            <Input secureTextEntry={true} onChangeText={this.handlePassword.bind(this)}
+                                   placeholder="Password"/>
                         </Item>
                         <Item>
-                            <Input onChangeText = {this.handleEmail.bind(this)}  placeholder="Email" />
+                            <Input onChangeText={this.handleEmail.bind(this)} placeholder="Email"/>
                         </Item>
-                        <Button style={{ margin: 10 }} onPress={() => {
+                        <Button style={{margin: 10}} onPress={() => {
                             postCall('users/create', {
-                               'user':{'username':this.state.username, 'password':this.state.password, 'email': this.state.email }
+                                'user': {
+                                    'username': this.state.username,
+                                    'password': this.state.password,
+                                    'email': this.state.email
+                                }
                             })
-                            .then(data => {console.log(data.status)})
-                            .catch(reason => console.log(reason.message));
+                                .then(data => {
+                                    console.log(data.status)
+                                })
+                                .catch(reason => console.log(reason.message));
                             this.props.navigation.navigate("SignIn");
                         }}
-                            primary rounded block>
+                                primary rounded block>
                             <Text> Sign Up</Text></Button>
-                        <Button style={{ margin: 10 }} onPress={() => {this.props.navigation.navigate("SignIn") }} success rounded block><Text> Sign In</Text></Button>
+                        <Button style={{margin: 10}} onPress={() => {
+                            this.props.navigation.navigate("SignIn")
+                        }} success rounded block><Text> Sign In</Text></Button>
                     </Form>
                 </Content>
             </Container>
