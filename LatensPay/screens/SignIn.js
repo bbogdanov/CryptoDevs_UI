83a1804f5264bf667659ca57167a1fb067a55b57
Tito Titov <tima_t@Titos-MacBook-Pro.local>
2018-04-01 09:14:09 +0300

    Added apy calls and auth service

--- LatensPay/screens/SignIn.js
+++ LatensPay/screens/SignIn.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
-import {View} from 'react-native';
-import bitcoin from '../img/bitcoin-logo.png';
 import { Container, Label, Form, Item, Content, Button, Input, Text } from 'native-base';
+import { postCall } from '../services/Http';
 import { onSignIn } from "../services/Auth";
-export default ({ navigation }) => (
-    <Container>
-        <Content>
-            <Form>
-                <Item>
-                    <Input placeholder="Username" />
-                </Item>
-                <Item last>
-                    <Input placeholder="Password" />
-                </Item>
-                <Button style={{margin:10}} onPress={() => {onSignIn(); console.log('here'); navigation.navigate("SignedIn")}} block rounded success><Text> Login</Text></Button>
-            </Form>
-        </Content>
-    </Container>
-);
+
+
+export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            password: '',
+            email: ''
+        }
+    }
+
+    handlePassword(password) {
+        this.setState({ password });
+    }
+
+    handleEmail(email) {
+        this.setState({ email });
+    }
+
+    render() {
+        return (
+            <Container>
+                <Content>
+                    <Form>
+                        <Item>
+                            <Input onChangeText={this.handleEmail.bind(this)} placeholder="Email" />
+                        </Item>
+                        <Item>
+                            <Input password onChangeText={this.handlePassword.bind(this)} placeholder="Password" />
+                        </Item>
+                        <Button style={{ margin: 10 }} onPress={() => {
+                            postCall('user_token', {
+                                'auth': { 'password': this.state.password, 'email': this.state.email }
+                            })
+                                .then(
+                                    (data) => {
+                                        onSignIn(data.jwt);
+                                        this.props.navigation.navigate("SignedIn");
+                                    })
+                                .catch(reason => console.log(reason.message));
+                        }}
+                            primary rounded block>
+                            <Text> Login</Text></Button>
+                    </Form>
+                </Content>
+            </Container>
+        )
+    }
+
+}
--- LatensPay/services/Http.js
+++ LatensPay/services/Http.js
@@ -1,15 +1,16 @@
 import { AsyncStorage } from "react-native";
 export const API_URL = "https://evening-springs-19512.herokuapp.com/";
-export const CONTENT_TYPE = "application/json";
+export const CONTENT_TYPE = "application/json; charset=utf-8";
 
 export const getCall = async (url, params) => {
     try {
-        const TOKEN = await AsyncStorage.getItem('token');
+        const TOKEN = await AsyncStorage.getItem('token') || -1;
         let response = await fetch(
             API_URL + url, {
                 headers: {
+                    'Accept': 'application/json',
                     'Authorization': 'Bearer ' + TOKEN,
-                    'content-type': CONTENT_TYPE,
+                    'Content-Type': CONTENT_TYPE 
                 },
                 method: 'GET'
             }
@@ -28,11 +29,11 @@ export const postCall = async (url, params) => {
             API_URL + url,
             {
                 body: JSON.stringify(params),
-                cache: 'no-cache',
-                credentials: 'same-origin', 
                 headers: {
+                    'Accept': CONTENT_TYPE ,
+                    'Access-Control-Allow-Rigin': '*',
                     'Authorization': 'Bearer ' + TOKEN,
-                    'content-type': CONTENT_TYPE,
+                    'Content-Type': CONTENT_TYPE
                 },
                 method: 'POST'
             }
