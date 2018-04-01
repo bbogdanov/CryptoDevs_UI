61532ffcabe4e41fe361fe943f6614dc964087ee
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-04-01 13:31:47 +0300

    Swipe disabled and refresh on navigation change introduced

--- LatensPay/navigation/RootNavigation.js
+++ LatensPay/navigation/RootNavigation.js
@@ -1,9 +1,7 @@
-import {
-  StackNavigator,
-  TabNavigator,
-  SwitchNavigator
-} from "react-navigation";
-import React, { Component } from 'react';
+import {StackNavigator, SwitchNavigator, TabNavigator} from "react-navigation";
+import React from 'react';
+import {Subject} from "rxjs";
+
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import SignUp from "../screens/SignUp";
@@ -15,94 +13,101 @@ import Withdraw from "../screens/Withdraw";
 import Send from "../screens/Send";
 
 const iconsStyle = {
-  fontSize: 30
+    fontSize: 30
 }
 
 export const SignedOut = StackNavigator({
-  SignUp: {
-      screen: SignUp,
-      navigationOptions: {
-          title: "Sign Up"
-      }
-  },
-  SignIn: {
-      screen: SignIn,
-      navigationOptions: {
-          title: "Sign In"
-      }
-  }
+    SignUp: {
+        screen: SignUp,
+        navigationOptions: {
+            title: "Sign Up"
+        }
+    },
+    SignIn: {
+        screen: SignIn,
+        navigationOptions: {
+            title: "Sign In"
+        }
+    }
 });
 
+export const navigationChangedObservable = new Subject();
+
 export const SignedIn = TabNavigator({
-Home: {
-  screen: Home
-},
-Deposit: {
-  screen: Deposit,
-},
-Withdraw: {
-  screen: Withdraw,
-},
-Send: {
-  screen: Send,
-},
-Profile: {
-  screen: Profile,
-}
+    Home: {
+        screen: Home
+    },
+    Deposit: {
+        screen: Deposit,
+    },
+    Withdraw: {
+        screen: Withdraw,
+    },
+    Send: {
+        screen: Send,
+    },
+    Profile: {
+        screen: Profile,
+    }
 }, {
-  tabBarPosition: 'bottom',
-  tabBarOptions: {
-      showLabel: false,
-      showIcon: true,
-      style: {
-        backgroundColor: '#4458ba',
-      }
-  },
-  navigationOptions: ({ navigation }) => ({
-      tabBarIcon: (props) => {
-        switch(navigation.state.routeName) {
-            case 'Home':
-                return (
-                    <Icon name='home' style={iconsStyle} />
-                );
-            case 'Deposit':
-                return (
-                    <Icon name='currency-usd-off' style={iconsStyle} />
-                );
-            case 'Withdraw':
-                return (
-                    <Icon name='chart-line' style={iconsStyle} />
-                );
-            case 'Send':
-                return (
-                    <Icon name='cube-send' style={iconsStyle} />
-                );
-            case 'Profile':
-                return (
-                    <Icon name='account-card-details' style={iconsStyle} />
-                );
-            default:
-              return null;
+    swipeEnabled: false,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+        showLabel: false,
+        showIcon: true,
+        style: {
+            backgroundColor: '#4458ba',
+        },
+    },
+    navigationOptions: ({navigation}) => ({
+        tabBarIcon: (props) => {
+            switch (navigation.state.routeName) {
+                case 'Home':
+                    return (
+                        <Icon name='home' style={iconsStyle}/>
+                    );
+                case 'Deposit':
+                    return (
+                        <Icon name='currency-usd-off' style={iconsStyle}/>
+                    );
+                case 'Withdraw':
+                    return (
+                        <Icon name='chart-line' style={iconsStyle}/>
+                    );
+                case 'Send':
+                    return (
+                        <Icon name='cube-send' style={iconsStyle}/>
+                    );
+                case 'Profile':
+                    return (
+                        <Icon name='account-card-details' style={iconsStyle}/>
+                    );
+                default:
+                    return null;
+            }
+        },
+        tabBarOnPress: (location) => {
+            navigationChangedObservable.next();
+            navigation.navigate(location.scene.route.key);
         }
-      }
-  }),
+    }),
 
 });
 
 export const createRootNavigator = (signedIn = false) => {
-  return SwitchNavigator(
-      {
-          SignedIn: {
-              screen: SignedIn
-          },
-          SignedOut: {
-              screen: SignedOut
-          }
-      },
-      {
-          initialRouteName: signedIn ? "SignedIn" : "SignedOut"
-      }
-  );
+    return SwitchNavigator(
+        {
+            SignedIn: {
+                screen: SignedIn
+            },
+            SignedOut: {
+                screen: SignedOut
+            }
+        },
+        {
+            initialRouteName: signedIn ? "SignedIn" : "SignedOut"
+        }
+    );
 };
 
 
